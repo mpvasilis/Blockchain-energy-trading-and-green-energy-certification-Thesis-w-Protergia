c2fd56b7ae1df33b12eac20a29c0718b5c372580
CH-Roumeliotis <christosroumeliotisfg@gmail.com>
2021-06-17 20:32:28 +0300

    Version 3

--- src/contracts/PPA.sol
+++ src/contracts/PPA.sol
@@ -1,13 +1,62 @@
 pragma solidity >=0.4.21 <0.7.0;
 pragma experimental ABIEncoderV2;
 
-contract PPA{
+contract PPAToken {
+
+    string public name = "PPA Token"; 
+    string public symbol = "PPA";
+    string public standard = "PPA v1.0";
+    uint256 public totalSupply;
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Claim(address _buyer, uint256 _amount);
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Not enough balance");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(
+            balanceOf[_from] >= _value,
+            "_from does not have enough tokens"
+        );
+        require(
+            allowance[_from][msg.sender] >= _value,
+            "Spender limit exceeded"
+        );
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
+
+contract PPA is PPAToken{
+
+    //PPAToken public tokenContract;
 
     enum Status {Pending, Approved, Rejected}
 
     struct ppa {
         address buyerID;
-        address producerID;
+        address payable producerID;
         uint energy;
         uint price;
         uint startDay;
@@ -18,19 +67,14 @@ contract PPA{
     mapping(string => ppa) ppas;
     ppa[] listOfPPAs;
 
-    address[] listOfUsers; //list of users who want to participate in ppa
-
-    function claimPPA() public {
-        listOfUsers.push(msg.sender);
-    }
-
-    function getAllUsers() public view returns(address[] memory){
-        return listOfUsers;
-    }
-
     function createPPA(address _buyerID, uint _energy, uint _price, uint _startDay, uint _endDay) public {
 
+        uint256 _tokenSupply = 1;
+
         require(_buyerID != msg.sender, "Wrong");
+
+        balanceOf[msg.sender] = _tokenSupply;
+        totalSupply = _tokenSupply;
 
         listOfPPAs.push(ppa({
             buyerID: _buyerID,
@@ -56,8 +100,25 @@ contract PPA{
         }
     }
 
-    function acceptPPA() public {
+    function claimPPA() public payable {
+
+        //uint256 numberOfToken
+        uint256 _numberOfPPA = 1;
         address buyerId = msg.sender;
+
+        require(
+            msg.value == _numberOfPPA * tokenPrice,
+            "Number of tokens does not match with the value"
+        );
+        /*require(
+            PPAToken.balanceOf(address(this)) >= _numberOfPPA,
+            "Contact does not have enough tokens"
+        );*/
+        require(
+            PPAToken.transfer(msg.sender, _numberOfPPA),
+            "Some problem with token transfer"
+        );
+
         for(uint i = 0; i<listOfPPAs.length; i++){
             require(listOfPPAs[i].producerID != msg.sender, "Wrong2");
             if((listOfPPAs[i].buyerID == buyerId)){
@@ -65,6 +126,7 @@ contract PPA{
                 listOfPPAs[i].status = Status.Approved;
             }
         }
+        emit Claim(buyerId, _numberOfPPA);
     }
 
     function denyPPA() public {
@@ -77,25 +139,7 @@ contract PPA{
         }
     }
 
-    /*function PPA(wPPA memory _ppa) public onlyRegisteredBattery {
-        for(uint i = 0; i<listOfppas.length; i++){
-            if(listOfppas[i].producerID == msg.sender){
-
-            }
-        }
-    }*/
-
     function viewAllWaitingPPAs () public view returns (ppa[] memory){
         return listOfPPAs;
     }
-
-    /*function getPPA(string memory _status) public view returns(ppa[] memory){
-        //require(_status = "Waiting" || _status = "Active", "it must be Waiting or Active only");
-
-        if(_status = "Active"){
-            return listOfppas[_status];
-        }else{
-            return listOfppas[_status];
-        }
-    }*/
 }
