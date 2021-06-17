0aa8847ad43a995fc96a853e1c1e4e0856bae9b8
CH-Roumeliotis <christosroumeliotisfg@gmail.com>
2021-06-17 20:32:49 +0300

    Version 3

--- src/contracts/ERC20.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.4.21 <0.7.0;
-
-contract ERC20 {
-
-    //function transfer(address _to, address _from) public returns (bool Succeed);
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    //function transferFrom(address _to, address _from, uint256 _amount) public returns (bool Succeed);
-}
