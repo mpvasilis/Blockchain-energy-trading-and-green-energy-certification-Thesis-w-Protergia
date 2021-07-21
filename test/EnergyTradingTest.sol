40dca67d47e6da0e8dcc52f562d4771ccc5428b1
CH-Roumeliotis <christosroumeliotisfg@gmail.com>
2021-07-21 12:52:12 +0300

    Corporate-Auctioning-Horizontal PPAs and PPA Energy Trading v2.0

--- test/EnergyTradingTest.sol
+++ test/EnergyTradingTest.sol
@@ -31,22 +31,20 @@ contract testEnergyTrading {
     }
 
     function testCreateEnergyBid() public {
-        uint dayExpected = 21102021;
         uint energyExpected = 1000;
         uint priceExpected = 10;
 
         address prosumerID;
-        uint day;
+        uint timestamp;
         uint energy;
         uint eprice;
         //note: this test is not working unless you removed the modifier var "onlyRegisteredBattery" from energy offer function.
 
         uint _idbid = 0;
         eb.energyOffer(dayExpected, energyExpected, priceExpected);
-        (prosumerID, day, energy, eprice) = eb.getBidsByLength(_idbid);
+        (prosumerID, timestamp, energy, eprice) = eb.getBidsByLength(_idbid);
         Assert.equal(energy, energyExpected, "Wrong result 3");
-        Assert.equal(day, dayExpected, "Wrong result 4");
-        Assert.equal(prosumerID, address(this), "Wrong result 5");
-        Assert.equal(eprice, priceExpected, "Wrong result 6");
+        Assert.equal(prosumerID, address(this), "Wrong result 4");
+        Assert.equal(eprice, priceExpected, "Wrong result 5");
     }
 }
