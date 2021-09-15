09365bd1b6e4ffac6723fa40cd4785539aea9fdf
CH-Roumeliotis <christosroumeliotisfg@gmail.com>
2021-09-15 17:20:21 +0300

    added RECs and comments for documentation

--- src/contracts/RECs/Issuer.sol
+++ src/contracts/RECs/Issuer.sol
@@ -17,7 +17,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-import "./Registry.sol";
+import "src/contracts/RECs/Registry.sol";
 
 /// @title Issuer contract
 /// @notice Used to manage the request/approve workflow for issuing ERC-1888 certificates.
