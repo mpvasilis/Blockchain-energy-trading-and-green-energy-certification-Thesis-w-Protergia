33a95d30615a7b3b7dcc6ea5fcda91b224e237fb
CH-Roumeliotis <christosroumeliotisfg@gmail.com>
2021-11-23 19:36:34 +0200

    POW-2 & POW-23 | Device manager & events

---
+++ src/contracts/Device.sol
@@ -0,0 +1,62 @@
+pragma solidity >=0.4.21 <0.9.0;
+
+contract Device {
+    event onDeviceAdded(address indexed ownerOfDevice, uint date, string id);
+    event onDeviceUpdated(address indexed ownerOfDevice, uint date, string id);
+    event onDeviceTransferOwnership(address oldOwner, address indexed newOwner);
+    event onDeviceRemoved(address device, uint date);
+
+    ///@notice Struct to store all devices 
+    ///@dev ownerOfDevice is the current address which the device is connected
+    ///@dev typeOfDevice, PV, EV, Solar, Biomass, Hydo Turbine or battery(Multiple)
+    ///@dev isExist to match the registered device with bool value.
+    struct device {
+        address ownerOfDevice;
+        string typeOfDevice;
+        uint timestamp;
+        bool isExist;
+    }
+
+    ///@notice Mapping address as key to struct battery with mapping name batteries
+    mapping (address => device) devices;
+
+    modifier onlyRegisteredDevice{
+         require(devices[msg.sender].isExist==true, "Only registered devices have access");
+         _;
+     }
+
+    ///@notice Add a battery by address.
+    ///@dev Only one device can register by each address.
+    function addDevice (string memory typeOfDevice) public {
+        require(devices[msg.sender].isExist == false, "Device details already added");
+        devices[msg.sender] = device(msg.sender, typeOfDevice, block.timestamp, true);
+        emit onDeviceAdded(msg.sender, block.timestamp, typeOfDevice);
+    }
+
+    ///@notice Change details of a battery.
+    ///@dev Only registered devices have access to this function.
+    function updateDevice(address ownerOfDevice, string memory typeOfDevice) public onlyRegisteredDevice {
+        devices[ownerOfDevice].typeOfDevice = typeOfDevice;
+        uint day = block.timestamp;
+        emit onDeviceUpdated(ownerOfDevice, day, typeOfDevice);
+    }
+
+    function transferOwnershipOfDevice(address oldOwner, address newOwner) public onlyRegisteredDevice {
+        require(oldOwner != newOwner, "The address you would like to change is the same");
+        devices[oldOwner].ownerOfDevice = newOwner;
+        emit onDeviceTransferOwnership(oldOwner, newOwner);
+    }
+
+    function removeDevice() public {
+        address currentAddr = msg.sender;
+        devices[currentAddr].isExist = false; ///@notice if you use msg.sender instead of the variable "currentAddr" it is not deleted
+        //delete devices[currentAddr];
+        emit onDeviceRemoved(msg.sender, block.timestamp);
+    }
+
+    ///@notice Get signle device details from specific address
+    function getDeviceByAddress() public view returns (address, string memory, uint){
+        address deviceID = msg.sender;
+        return (devices[deviceID].ownerOfDevice, devices[deviceID].typeOfDevice, devices[deviceID].timestamp);
+    }
+}
